845e559266ca347c3219beaf4a842785badb7928
oklookat <>
2023-07-03 01:28:38 +0300

    тупо

--- service.go
+++ service.go
@@ -10,7 +10,6 @@ func wrapErrStr(err string) error {
 	return fmt.Errorf(_errPrefix+"%s", err)
 }
 
-// Генерирует ULID.
 func generateDeviceID() string {
 	return strconv.FormatInt(time.Now().UnixMilli(), 10)
 }
